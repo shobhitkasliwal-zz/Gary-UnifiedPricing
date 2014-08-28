9a5ff99164215913972efd98b8baa4c0ec3b979b
shobhitkasliwal <shobhit.kasliwal@gmail.com>
2014-08-28 12:28:13 -0500

    updates for the customer name.

--- Unified Pricing Sources/Unified Price for Var/frmReports.cs
+++ Unified Pricing Sources/Unified Price for Var/frmReports.cs
@@ -233,7 +233,7 @@ namespace Unified_Price_for_Var
         {
             Cursor.Current = Cursors.WaitCursor;
             Db.NonQuery("DELETE FROM tblByItem_Report");
-            String DefaultQuery = "INSERT INTO tblByItem_Report ( [Item Number], [Item Description], [Customer Number], [Current Price], InDistrGroup, QuoteDate, Last12MonthQTY ) SELECT tblPricing.[item Number], tblPricing.[item description], tblCustomers.[Customer Number], tblPricing.[Current Price], '' AS InDistrGroup, tblPricing.QuoteDate, tblPricing.Last12MonthQty FROM tblPricing  INNER JOIN tblCustomers ON tblPricing.[Customer Number] = tblCustomers.[Customer Number] ";
+            String DefaultQuery = "INSERT INTO tblByItem_Report ( [Item Number], [Item Description], [Customer Name], [Customer Number], [Current Price], InDistrGroup, QuoteDate, Last12MonthQTY ) SELECT tblPricing.[item Number], tblPricing.[item description], tblCustomers.[Customer Bill Name],tblCustomers.[Customer Number], tblPricing.[Current Price], '' AS InDistrGroup, tblPricing.QuoteDate, tblPricing.Last12MonthQty FROM tblPricing  INNER JOIN tblCustomers ON tblPricing.[Customer Number] = tblCustomers.[Customer Number] ";
 
             if (rdoPrintByItemNumb.Checked)
             {
@@ -318,7 +318,7 @@ namespace Unified_Price_for_Var
                 //            );
                 //    }
                 //}
-                Db.NonQuery("INSERT INTO tblByItem_Report ( [Item Number], [Item Description], [Customer Number], [Current Price], InDistrGroup, QuoteDate, Last12MonthQTY ) SELECT tblPricing.[item Number], tblPricing.[item description], tblCustomers.[Customer Number], tblPricing.[Current Price], '' AS InDistrGroup, tblPricing.QuoteDate, tblPricing.Last12MonthQty FROM (tblPricing INNER JOIN tblItems ON tblPricing.[Item Number] = tblItems.[Item Number])  INNER JOIN tblCustomers ON tblPricing.[Customer Number] = tblCustomers.[Customer Number] where tblItems.[Family] = '{0}'", cmbFamily.SelectedValue);
+                Db.NonQuery("INSERT INTO tblByItem_Report ( [Item Number], [Item Description], [Customer Name], [Customer Number], [Current Price], InDistrGroup, QuoteDate, Last12MonthQTY ) SELECT tblPricing.[item Number], tblPricing.[item description], tblCustomers.[Customer Bill Name], tblCustomers.[Customer Number], tblPricing.[Current Price], '' AS InDistrGroup, tblPricing.QuoteDate, tblPricing.Last12MonthQty FROM (tblPricing INNER JOIN tblItems ON tblPricing.[Item Number] = tblItems.[Item Number])  INNER JOIN tblCustomers ON tblPricing.[Customer Number] = tblCustomers.[Customer Number] where tblItems.[Family] = '{0}'", cmbFamily.SelectedValue);
                 Cursor.Current = Cursors.Default;
                 ReportViewers.frmByItem_Viewer frm = new ReportViewers.frmByItem_Viewer();
                 frm.Text = "By Family Report";
@@ -331,7 +331,7 @@ namespace Unified_Price_for_Var
             if (rdoPrintAll.Checked)
             {
                 //var reallyItems = Db.ExecuteDataTable("SELECT * FROM tblItems");
-                Db.NonQuery("INSERT INTO tblByItem_Report ( [Item Number], [Item Description], [Customer Number], [Current Price], InDistrGroup, QuoteDate, Last12MonthQTY ) SELECT tblItems.[item Number], tblItems.[item description], tblCustomers.[Customer Number], tblPricing.[Current Price], '' AS InDistrGroup, tblPricing.QuoteDate, tblPricing.Last12MonthQty FROM (tblItems INNER JOIN tblPricing ON tblItems.ID = tblPricing.ID) INNER JOIN tblCustomers ON tblPricing.[Customer Number] = tblCustomers.[Customer Number];");
+                Db.NonQuery("INSERT INTO tblByItem_Report ( [Item Number], [Item Description], [Customer Name], [Customer Number], [Current Price], InDistrGroup, QuoteDate, Last12MonthQTY ) SELECT tblItems.[item Number], tblItems.[item description], tblCustomers.[Customer Bill Name], tblCustomers.[Customer Number], tblPricing.[Current Price], '' AS InDistrGroup, tblPricing.QuoteDate, tblPricing.Last12MonthQty FROM (tblItems INNER JOIN tblPricing ON tblItems.ID = tblPricing.ID) INNER JOIN tblCustomers ON tblPricing.[Customer Number] = tblCustomers.[Customer Number];");
                 //foreach (DataRow itemRow in reallyItems.Rows)
                 //{
                 //    var items = Db.ExecuteDataTable("SELECT * FROM tblPricing WHERE [Item Number] = '{0}' ORDER BY [Current Price] DESC", itemRow["Item Number"]);
@@ -385,7 +385,7 @@ namespace Unified_Price_for_Var
                 //    }
                 //}
 
-                Db.NonQuery("INSERT INTO tblByItem_Report ( [Item Number], [Item Description], [Customer Number], [Current Price], InDistrGroup, QuoteDate, Last12MonthQTY ) SELECT tblPricing.[item Number], tblPricing.[item description], tblCustomers.[Customer Number], tblPricing.[Current Price], '' AS InDistrGroup, tblPricing.QuoteDate, tblPricing.Last12MonthQty FROM (tblPricing INNER JOIN tblItems ON tblPricing.[Item Number] = tblItems.[Item Number])  INNER JOIN tblCustomers ON tblPricing.[Customer Number] = tblCustomers.[Customer Number] where tblCustomers.[Customer Number] = '{0}' and tblItems.[Family] = '{1}' ", cmbCust1.SelectedValue, cmbFamily1.SelectedValue);
+                Db.NonQuery("INSERT INTO tblByItem_Report ( [Item Number], [Item Description], [Customer Name], [Customer Number], [Current Price], InDistrGroup, QuoteDate, Last12MonthQTY ) SELECT tblPricing.[item Number], tblPricing.[item description], tblCustomers.[Customer Bill Name], tblCustomers.[Customer Number], tblPricing.[Current Price], '' AS InDistrGroup, tblPricing.QuoteDate, tblPricing.Last12MonthQty FROM (tblPricing INNER JOIN tblItems ON tblPricing.[Item Number] = tblItems.[Item Number])  INNER JOIN tblCustomers ON tblPricing.[Customer Number] = tblCustomers.[Customer Number] where tblCustomers.[Customer Number] = '{0}' and tblItems.[Family] = '{1}' ", cmbCust1.SelectedValue, cmbFamily1.SelectedValue);
               
                 Cursor.Current = Cursors.Default;
                 ReportViewers.frmItemsByCustFamily_Viewer frm = new ReportViewers.frmItemsByCustFamily_Viewer();
